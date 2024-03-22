13c862987c42b86507dd852b91090c319c6640f7
Kais <kaisdewani@gmail.com>
2024-03-21 20:26:41 -0500

    refactored and added linkedin desktop icon

--- components/DesktopIcons.js
+++ components/DesktopIcons.js
@@ -1,26 +1,40 @@
-import { Joy102 } from "@react95/icons";
+import { Joy102, Inetcfg2300 } from "@react95/icons";
 
 export default function DesktopIcons() {
-  const handleClickTieriflicks = () => {
-    const url = "https://apps.apple.com/us/app/tieriflicks/id6476421455";
-    window.open(url, "_blank");
+  const tieriflicksAppUrl = "https://apps.apple.com/us/app/tieriflicks/id6476421455";
+  const linkedInUrl = "https://www.linkedin.com/in/kaisdewani/"
+
+  const handleIconClick = (passURL) => () => {
+    window.open(passURL, "_blank");
   };
+  
 
   return (
     <div className="mt-1">
+      {/* iOS App Icon */}
       <div className="block m-auto px-6">
         <Joy102
-          onClick={handleClickTieriflicks}
+          onClick={handleIconClick(tieriflicksAppUrl)}
           variant="32x32_4"
           className="cursor-pointer"
         />
       </div>
-      <p className="mt-1 px-1 text-lg font-semibold">Tieriflicks</p>{" "}
-      {/* Main Title */}
-      <p className="text-sm font-normal text-gray-600">
+      <p className="mt-1 px-1 text-lg font-semibold">Tieriflicks</p>
+      <p className="text-sm pb-5 font-normal text-gray-600">
         My First iOS App!
-      </p>{" "}
-      {/* Subheading */}
+      </p>
+      {/* LinkedIn Icon  */}
+      <div className="block m-auto px-6">
+        <Inetcfg2300
+          onClick={handleIconClick(linkedInUrl)}
+          variant="32x32_4"
+          className="cursor-pointer"
+        />
+      </div>
+      <p className="mt-1 px-1 text-lg font-semibold">LinkedIn</p>
+      <p className="text-sm font-normal text-gray-600">
+        Lets Connect
+      </p>
     </div>
   );
 }
